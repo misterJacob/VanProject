a0a4297a622216c3fdf743c222ccb9163d02e1b5
misterJacob <beckjacob56@gmail.com>
2023-08-10 20:54:30 +0100

    ready for

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import { useRouteError } from 'react-router-dom';
+import React from "react";
+import { useRouteError } from "react-router-dom";
 
 export default function Error() {
-  const error = useRouteError()
-  console.log(error)
-  return<div>
-     <h1>{error.message}</h1>;
-     
-  </div>
+  const error = useRouteError();
+  console.log(error);
+  const style = {
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  };
+  return (
+    <div style={style}>
+      <h1>{error.message}</h1>
+      <h2>
+        <pre>{error.message}</pre>
+      </h2>
+      <h3>
+        <pre> {error.statusText}</pre>{" "}
+      </h3>
+    </div>
+  );
 }
-
